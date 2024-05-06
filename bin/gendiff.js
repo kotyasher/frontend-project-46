c05651408f720f2ccd056194e0931943713550d4
Yulia Mishieva <kotyasher@gmail.com>
2024-05-06 17:24:03 +0300

    Fix Third Step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,24 +1,16 @@
-#!/usr/bin/env node
-
 import { Command } from 'commander';
-import getParseFile from '../src/index.js';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-
-program.command('first', 'second', 'option')
-  .option('-f, --format <type>', 'output format')
-  .argument('<first>', 'path to file 1')
-  .argument('<second>', 'path to file 2')
-  .action((first, second, option = 'JSON') => {
-    const parsedFile1 = getParseFile(first);
-    console.log(parsedFile1);
-    const parsedFile2 = getParseFile(second);
-    console.log(parsedFile2);
-    console.log(option);
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .argument('<filepath2>')
+  .action((filePath1, filePath2) => {
+    console.log(genDiff(filePath1, filePath2));
   });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import { realpathSync, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
+import path from 'path';
 
-const getParseFile = (path) => {
-  const absPathFile = realpathSync(path);
-  if (!absPathFile.endsWith('json')) {
-    return null;
-  }
-  return JSON.parse(readFileSync(absPathFile));
+const genDiff = (filePath1, filePath2) => {
+  const pathFile1 = path.resolve(process.cwd(), filePath1);
+  const pathFile2 = path.resolve(process.cwd(), filePath2);
+
+  const fileReading1 = readFileSync(pathFile1, 'utf-8');
+  const fileReading2 = readFileSync(pathFile2, 'utf-8');
+
+  const json1 = JSON.parse(fileReading1);
+  const json2 = JSON.parse(fileReading2);
+
+  return [json1, json2];
 };
 
-export default getParseFile;
+export default genDiff;
